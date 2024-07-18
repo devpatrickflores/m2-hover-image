0593bdd5918dd2722e9fe1a52aee1070dee19b56
Patrick Flores <patrickflores.dev@gmail.com>
2024-07-19 01:38:50 +1000

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Patrick Flores. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'PF_HoverImage', __DIR__);
